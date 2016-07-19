59cd04fe93f554e74e72d5796b21085432a61818
John Chiotellis <chiotell@in.tum.de>
2016-07-19 11:14:12 +0200

    add meshlab and bash script for downsampling

---
+++ scripts/downsample_meshes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+root_dir=/projects/csd_lmnn;
+
+mlx_script=${root_dir}/scripts/QECD_20k.mlx
+shapes_in_dir=${root_dir}/data/SHREC14_HUMAN/REAL/original_meshes
+shapes_out_dir=${root_dir}/data/SHREC14_HUMAN/REAL/resol_20kf
+
+mkdir -p $shapes_out_dir
+cd $shapes_in_dir
+
+for i in $( ls ); 
+do
+	echo Downsampling shape $i
+	meshlabserver -i $shapes_in_dir"/"$i -o $shapes_out_dir"/"$i -s $mlx_script
+done
